6e77cf3ddc7795b631bfa844ae61ec9caf9d4104
Minh Vy ha <minhvy.ha@outlook.com>
2022-09-10 21:41:33 +1000

    update

--- server/index.js
+++ server/index.js
@@ -1,3 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+
+const app = express()
+const port = 3001
+
+app.use(cors)
+app.use(express.json())
+
+app.listen(port, () =>{
+    console.log('app listening')
+})
