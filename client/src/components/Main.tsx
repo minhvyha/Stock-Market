fe900bea252ca99e413fc211d7046067d338bf0a
Minh Vy ha <minhvy.ha@outlook.com>
2022-09-11 22:08:19 +1000

    update

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Line } from 'react-chartjs-2'
+import { data } from '../data'
 
 function Main() {
+  const [chartData, setChartData] = useState();
+
+  var API_KEY = 'UH9YQHN45N2JZGYW'
+  var api = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=${API_KEY}`
+
+  useEffect(() =>{
+    setChartData({})
+  }, [])
   return (
     <div>
-      <Line />
+      {/* <Line data={} options={} /> */}
     </div>
   )
 }
