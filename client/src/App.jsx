2c8f8fa2c389cb04308f10d5a3b0e6dc883d0c39
Minh Vy ha <minhvy.ha@outlook.com>
2022-09-14 17:28:35 +1000

    update

---
+++ client/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
+import PieChart from "./components/PieChart";
+import { UserData } from "./Data";
+
+function App() {
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: UserData.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+
+  // IF YOU SEE THIS COMMENT: I HAVE GOOD EYESIGHT
+
+  return (
+    <div className="App">
+      <div style={{ width: 700 }}>
+        <BarChart chartData={userData} />
+      </div>
+      <div style={{ width: 700 }}>
+        <LineChart chartData={userData} />
+      </div>
+      <div style={{ width: 700 }}>
+        <PieChart chartData={userData} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- client2/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-import { App } from './App';
-
-const rootElement = document.getElementById('root') as HTMLElement;
-createRoot(rootElement).render(<App />);
