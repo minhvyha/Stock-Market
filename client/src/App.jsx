63c4e5506d1f92cc3e3a3d1732052f2a39c384f4
Minh Vy ha <minhvy.ha@outlook.com>
2022-09-23 15:34:03 +1000

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,60 +6,31 @@ import { UserData } from "./Data";
 import { Data } from "./Data1";
 
 function App() {
-  const [newData, setNewData] = useState();
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.year),
-    datasets: [
-      {
-        label: "Users Gained",
-        data: UserData.map((data) => data.userGain),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-  });
+  console.log('start')
+  const [newData, setNewData] = useState(fetchData);
 
-  useEffect(() => {
+  function fetchData(){
     let labels = [];
     let data = [];
     let dataYear = Data["Time Series (Daily)"];
     for (const [key, val] of Object.entries(dataYear)) {
       labels.push(key);
-      data.push((val["1. open"] + val["3. low"]) / 2);
+      data.push((parseFloat(val["1. open"]) + parseFloat(val["3. low"])) / 2);
     }
     let datasets = [
       {
-        labels: "Price",
+        label: "Price",
         data: data,
-        backgroundColor: ["black"],
+        backgroundColor: ["#ef8e19"],
         borderColor: "black",
         borderWidth: 2,
       },
     ];
-    setNewData({
-      labels,
-      datasets
-    });
-  }, []);
+    return {labels, datasets}
+  }
 
   return (
     <div className="App">
-      <div style={{ width: 700 }}>
-        <BarChart chartData={userData} />
-      </div>
-      <div style={{ width: 700 }}>
-        <LineChart chartData={userData} />
-      </div>
-      <div style={{ width: 700 }}>
-        <PieChart chartData={userData} />
-      </div>
       <div style={{ width: 700 }}>
         <LineChart chartData={newData} />
       </div>
