20c24340d20e5a9b7faeaccbb1b5914d8d1c2b11
Minh Vy ha <minhvy.ha@outlook.com>
2022-09-23 17:34:32 +1000

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,73 @@
-import { useState, useEffect } from "react";
-import BarChart from "./components/BarChart";
-import LineChart from "./components/LineChart";
-import PieChart from "./components/PieChart";
-import { UserData } from "./Data";
+import React, { useState, useEffect } from "react";
 import { Data } from "./Data1";
+import {
+  Chart,
+  LineElement,
+  PointElement,
+  LineController,
+  CategoryScale,
+  LinearScale,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+Chart.register(
+  LineElement,
+  PointElement,
+  LineController,
+  CategoryScale,
+  LinearScale,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+);
 
 function App() {
-  console.log("start");
-  const [newData, setNewData] = useState(fetchData());
+  const [data, setData] = useState();
+  const [option, setOption] = useState();
 
-  function fetchData() {
-    let labels = [];
-    let data = [];
-    let dataYear = Data["Time Series (Daily)"];
-    for (const [key, val] of Object.entries(dataYear)) {
-      labels.push(key);
-      data.push((parseFloat(val["1. open"]) + parseFloat(val["3. low"])) / 2);
+  useEffect(() => {
+    function fetchData() {
+      let labels = [];
+      let data = [];
+      let dataYear = Data["Time Series (Daily)"];
+      for (const [key, val] of Object.entries(dataYear)) {
+        labels.push(key);
+        data.push((parseFloat(val["1. open"]) + parseFloat(val["3. low"])) / 2);
+      }
+      let datasets = [
+        {
+          label: "Price",
+          data: data,
+          backgroundColor: ["#ef8e19"],
+          borderColor: "black",
+          borderWidth: 2,
+        },
+      ];
+      return { labels, datasets };
     }
-    let datasets = [
-      {
-        label: "Price",
-        data: data,
-        backgroundColor: ["#ef8e19"],
-        borderColor: "black",
-        borderWidth: 2,
+    setData(fetchData());
+    setOption({
+      responsive: true,
+      plugins: {
+        legend: {
+          display: false,
+        },
+        title: {
+          display: true,
+          text: "test1",
+        },
       },
-    ];
-    return { labels, datasets };
-  }
-
+    });
+  }, []);
   return (
     <div className="App">
-      <div style={{ width: 700 }}>
-        <LineChart chartData={newData} />
+      <div style={{width: 700}}>
+      <Line options={option} data={data} />
       </div>
     </div>
   );
--- client/src/components/LineChart.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Line } from "react-chartjs-2";
-
-function LineChart({ chartData }) {
-  return <Line data={chartData} />;
-}
-
-export default LineChart;
