a08d21a442d9b9242a586de0a6cae8dda29ea898
Minh Vy ha <minhvy.ha@outlook.com>
2022-09-24 12:30:49 +1000

    update google oauth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,12 +18,12 @@ function App() {
     let userObject = jwt_decode(response.credential);
     console.log(userObject);
     setUser(userObject);
-    document.getElementById("signInDiv").hidden = true
+    document.getElementById("signInDiv").hidden = true;
   }
 
-  function handleSignOut(event){
-    setUser({})
-    document.getElementById("signInDiv").hidden = false
+  function handleSignOut(event) {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
   }
 
   useEffect(() => {
@@ -86,20 +86,16 @@ function App() {
       },
     });
   }, []);
-  console.log(user)
+  console.log(user);
   return (
     <div className="App">
-      {Object.keys(user).length !== 0 ? (
-        <div className="asdf">
-          <div id="signInDiv"></div>
-          <h3>{user.name}</h3>
-          <h1>asdf</h1>
+      <div id="signInDiv"></div>
+      {Object.keys(user).length !== 0 && (
+        <div>
+          <LineChart options={options} data={data} />
           <button onClick={handleSignOut}>Sign Out</button>
         </div>
-      ) : (
-        <div id="signInDiv"></div>
       )}
-      {/* <LineChart options={options} data={data} /> */}
     </div>
   );
 }
