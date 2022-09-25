79c6e22db3061679c577c6addbf702ea62524f22
Minh Vy ha <minhvy.ha@outlook.com>
2022-09-25 10:44:28 +1000

    update

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const passportSetup = require('./passport')
+const passportSetup = require("./passport");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 
