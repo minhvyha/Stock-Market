485866694aa6c00a4d7c43bccfaf54aaaaec2c97
Minh Vy ha <minhvy.ha@outlook.com>
2022-09-25 23:03:29 +1000

    update

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,30 +1,29 @@
 const router = require("express").Router();
 const passport = require("passport");
 
-router.get("/login/success", (req, res) =>{
-    if (req.user) {
-        res.status(200).json({
-            error:false,
-            message: "Successfully Loged In",
-            user: req.user
-        })
-    }
-    else{
-        res.status(403).json({error:true, message: "Not Authorized"})
-    }
-})
+router.get("/login/success", (req, res) => {
+  if (req.user) {
+    res.status(200).json({
+      error: false,
+      message: "Successfully Loged In",
+      user: req.user,
+    });
+  } else {
+    res.status(403).json({ error: true, message: "Not Authorized" });
+  }
+});
 
-router.get("/login/failed", (req,res) =>{
-    res.status(401).json({
-        error: true,
-        message: "Log in failure"
-    })
-})
+router.get("/login/failed", (req, res) => {
+  res.status(401).json({
+    error: true,
+    message: "Log in failure",
+  });
+});
 
 router.get(
-    "/google/callback",
-    passport.authenticate("google", {
-        successRedirect: process.env.CLIENT_URL,
-        failureRedirect: '/login/failed'
-    })
-)
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: process.env.CLIENT_URL,
+    failureRedirect: "/login/failed",
+  })
+);
