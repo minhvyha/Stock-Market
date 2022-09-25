7e333516cb06a1152d9a2dc010a3c721ac0df63b
Minh Vy ha <minhvy.ha@outlook.com>
2022-09-25 10:41:39 +1000

    update

--- server/passport.js
+++ server/passport.js
@@ -2,13 +2,15 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const passport = require("passport");
 
 passport.use(
-  new GoogleStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "/auth/google/callback",
-    scope: ["profile, email"]
-  },
-  function (accessToken, refreshToken, profile, callback){
-    callback(null, profile)
-  })
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+      scope: ["profile, email"],
+    },
+    function (accessToken, refreshToken, profile, callback) {
+      callback(null, profile);
+    }
+  )
 );
