21333a1e617d90be8e45f5a64c56f2baf791269b
Minh Vy ha <minhvy.ha@outlook.com>
2022-09-25 10:33:30 +1000

    update

--- server/index.js
+++ server/index.js
@@ -1,21 +1,23 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
-const cookieSession = require("cookie-session")
-const passport = require('passport')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 
-const app = express()
-const port = 3001
+const app = express();
+const port = 3001;
 
-app.use(cors)
-app.use(cookieSession({
+app.use(cors);
+app.use(
+  cookieSession({
     name: "session",
     keys: ["stock-market"],
-    maxAge: 24 * 60 * 60 * 100
-}))
-app.use(express.json())
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
+app.use(express.json());
 
-app.listen(port, () =>{
-    console.log('app listening')
-})
+app.listen(port, () => {
+  console.log("app listening");
+});
