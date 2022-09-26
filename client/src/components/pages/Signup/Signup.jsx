87ed01480ee77ef34c2161740f3453fdf699f7b0
Minh Vy ha <minhvy.ha@outlook.com>
2022-09-26 21:52:24 +1000

    update

--- client/src/components/pages/Signup/Signup.jsx
+++ client/src/components/pages/Signup/Signup.jsx
@@ -10,22 +10,24 @@ export default function Signup() {
   };
   return (
     <div className={styles.container}>
-      <h1 className={styles.heading}>Log In Form</h1>
+      <h1 className={styles.heading}>Sign Up Form</h1>
       <div className={styles.form.container}>
         <div className={styles.left}>
-          <img className={styles.img} src="./images/login.jpg" alt="login" />
+          <img className={styles.img} src="./images/signup.jpg" alt="login" />
         </div>
         <div className={styles.right}>
+          <h2 className={styles.form_heading}>Create Account</h2>
+          <input type="text" className={styles.input} placeholder="Username" />
           <input type="text" className={styles.input} placeholder="Email" />
-          <input type="text" className={styles.input} placeholder="Password" />
-          <button className={styles.btn}>Log In</button>
+          <input type="password" className={styles.input} placeholder="Password" />
+          <button className={styles.btn}>Sign Up</button>
           <p className={styles.text}>or</p>
           <button className={styles.google_btn} onClick={googleAuth}>
             <img src="./images/google.png" alt="google.icon" />
-            <span>Sign in with Google</span>
+            <span>Sign up with Google</span>
           </button>
           <p className={styles.text}>
-            New Here? <Link to="/signup">Sign Up</Link>
+            Already Have Account? <Link to="/login">Log In</Link>
           </p>
         </div>
       </div>
