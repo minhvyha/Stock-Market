d863422f4b52822a5e97d4cf4d0ed8005ae7b5ec
Minh Vy Ha <vyha0512@gmail.com>
2022-09-27 10:43:21 +1000

    update

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -10,29 +10,18 @@ export default function Home(userDetails) {
   };
   return (
     <div className={styles.container}>
-      <h1 className={styles.heading}>Sign Up Form</h1>
+      <h1 className={styles.heading}>Home</h1>
       <div className={styles.form.container}>
         <div className={styles.left}>
-          <img className={styles.img} src="./images/signup.jpg" alt="login" />
+          <img className={styles.img} src="./images/profile.jpg" alt="profile" />
         </div>
         <div className={styles.right}>
-          <h2 className={styles.form_heading}>Create Account</h2>
-          <input type="text" className={styles.input} placeholder="Username" />
-          <input type="text" className={styles.input} placeholder="Email" />
-          <input
-            type="password"
-            className={styles.input}
-            placeholder="Password"
-          />
-          <button className={styles.btn}>Sign Up</button>
-          <p className={styles.text}>or</p>
-          <button className={styles.google_btn} onClick={googleAuth}>
-            <img src="./images/google.png" alt="google.icon" />
-            <span>Sign up with Google</span>
-          </button>
-          <p className={styles.text}>
-            Already Have Account? <Link to="/login">Log In</Link>
-          </p>
+          <h2 className={styles.form_heading}>Profile</h2>
+          <img className={styles.profile_img} src={user.picture} alt="profile" />
+          <input type="text" defaultValue={user.nam} className={styles.input} placeholder="Username" />
+          <input type="text" defaultValue={user.email} className={styles.input} placeholder="Email" />
+          <button className={styles.btn} onClick={logout} >Log Out</button>
+    
         </div>
       </div>
     </div>
