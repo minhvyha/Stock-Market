36162d4887d88ae9dae30a060ef223c30bbe4c78
Minh Vy ha <minhvy.ha@outlook.com>
2022-09-29 13:29:56 +1000

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Symbol } from "./SP500";
-import axios from 'axios'
+import axios from "axios";
 import LineChart from "./components/LineChart";
 import jwt_decode from "jwt-decode";
 
 var dataOption = Symbol.map((company) => {
-  return (
-    <option value={company.Symbol}>{company.Name}</option>
-  )
-})
-
+  return <option value={company.Symbol}>{company.Name}</option>;
+});
 
 function App() {
   const [user, setUser] = useState({});
@@ -42,55 +39,51 @@ function App() {
       size: "large",
     });
   }, []);
-
-  useEffect(() => {
-    async function fetchData() {
-      const apiData = await fetch(
-        `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=${process.env.REACT_APP_API_KEY}`
-      );
-      const convertData = await apiData.json();
-      let labels = [];
-      let data = [];
-      let dataYear = await convertData["Time Series (Daily)"];
-      let symbol = await convertData["Meta Data"]["2. Symbol"]
-      console.log(symbol)
-      for (const [key, val] of Object.entries(dataYear)) {
-        labels.push(key);
-        data.push((parseFloat(val["1. open"]) + parseFloat(val["3. low"])) / 2);
-      }
-      let datasets = [
-        {
-          label: "Price",
-          data: data,
-          backgroundColor: ["#ef8e19"],
-          borderColor: "black",
-          borderWidth: 2,
-        },
-      ];
-      setData({ labels, datasets });
-      setOption({
-        tension: 0.2,
-        responsive: true,
-        plugins: {
-          tooltip: {
-            interaction: {
-              mode: "index",
-              axis: "x",
-            },
-            intersect: false,
-          },
-          legend: {
-            display: false,
-          },
-          title: {
-            display: true,
-            text: symbol,
-          },
-        },
-      });
+  async function fetchData(symbol) {
+    const apiData = await fetch(
+      `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    const convertData = await apiData.json();
+    let labels = [];
+    let data = [];
+    let dataYear = await convertData["Time Series (Daily)"];
+    for (const [key, val] of Object.entries(dataYear)) {
+      labels.push(key);
+      data.push((parseFloat(val["1. open"]) + parseFloat(val["3. low"])) / 2);
     }
-    fetchData();
-    
+    let datasets = [
+      {
+        label: "Price",
+        data: data,
+        backgroundColor: ["#ef8e19"],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ];
+    setData({ labels, datasets });
+    setOption({
+      tension: 0.2,
+      responsive: true,
+      plugins: {
+        tooltip: {
+          interaction: {
+            mode: "index",
+            axis: "x",
+          },
+          intersect: false,
+        },
+        legend: {
+          display: false,
+        },
+        title: {
+          display: true,
+          text: symbol,
+        },
+      },
+    });
+  }
+  useEffect(() => {
+    fetchData("AAPL");
   }, []);
   return (
     <div className="App">
@@ -107,9 +100,7 @@ function App() {
             id="symbolList"
             list="symbolData"
           />
-          <datalist id="symbolData">
-            {dataOption}
-          </datalist>
+          <datalist id="symbolData">{dataOption}</datalist>
 
           <button className="btn-sign-out" onClick={handleSignOut}>
             Sign Out
