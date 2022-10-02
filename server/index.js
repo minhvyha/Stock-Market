ab8a734821f2fb00616cbaeb2fd26c9b1f49fff4
Minh Vy ha <minhvy.ha@outlook.com>
2022-10-02 17:47:24 +1100

    update

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 app.use(
   cors({
-    origin: "https://localhost:3000",
-    methods: "GET,POST,PUT,DELETE",
+    origin: 'https://localhost:3000',
+    methods: 'GET,POST,PUT,DELETE',
     credentials: true,
   })
 );
@@ -16,5 +16,5 @@ app.use(express.json());
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log("app listening");
+  console.log('app listening');
 });
