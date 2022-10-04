efa27d5ee3d96bc5acc0d757afb89b72f480367c
Minh Vy ha <minhvy.ha@outlook.com>
2022-10-04 15:07:56 +1100

    updat

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <nav>
+      <input type="checkbox" id="check" />
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <label class="logo">DesignX</label>
+      <ul>
+        <li><a class="active" href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Feedback</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
