64910f7fecbb096dd7161b9dc6d00779edd6ee07
Minh Vy ha <minhvy.ha@outlook.com>
2022-10-09 12:40:46 +1100

    update

--- client/src/pages/ProtectedRoute.jsx
+++ client/src/pages/ProtectedRoute.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import {Navigate} from 'react-router-dom'
+import React from 'react';
+import { Navigate } from 'react-router-dom';
 
-function ProtectedRoute({user, children}) {
-    return Object.keys(user).length === 0 ? <Navigate to='/login' /> : children
+function ProtectedRoute({ user, children }) {
+  return Object.keys(user).length === 0 ? <Navigate to="/login" /> : children;
 }
 
-export default ProtectedRoute
+export default ProtectedRoute;
