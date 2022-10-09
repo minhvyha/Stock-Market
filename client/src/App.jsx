1a917072dbe14eb8778178081d18a64847364b08
Minh Vy ha <minhvy.ha@outlook.com>
2022-10-09 21:50:15 +1100

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,122 +10,107 @@ import ShareLayout from './pages/ShareLayout';
 import Intro from './pages/Intro';
 
 var dataOption = Symbol.map((company) => {
-  return <option value={company.Symbol}>{company.Name}</option>;
+	return <option value={company.Symbol}>{company.Name}</option>;
 });
 // https://cloud.iexapis.com/stable/stock/AAPL/quote?token=
 
 export const MainPageContext = React.createContext();
 
 function App() {
-  const [user, setUser] = useState({});
-  const [data, setData] = useState({
-    datasets: [],
-  });
-  const [options, setOption] = useState();
-  const [stock, setStock] = useState('AAPL');
-  function handleSignOut(event) {
-    setUser({});
-  }
+	const [user, setUser] = useState({});
+	const [data, setData] = useState({
+		datasets: [],
+	});
+	const [options, setOption] = useState();
+	const [stock, setStock] = useState('AAPL');
+	function handleSignOut(event) {
+		setUser({});
+	}
 
-  async function fetchData() {
-    const apiData = await fetch(
-      `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock}&apikey=${process.env.REACT_APP_API_KEY}`
-    );
-    const convertData = await apiData.json();
-    let labels = [];
-    let data = [];
-    let dataYear = await convertData['Time Series (Daily)'];
-    for (const [key, val] of Object.entries(dataYear)) {
-      labels.unshift(key);
-      data.unshift(
-        (parseFloat(val['1. open']) + parseFloat(val['3. low'])) / 2
-      );
-    }
-    let datasets = [
-      {
-        label: 'Price',
-        data: data,
-        backgroundColor: ['#ef8e19'],
-        borderColor: '#006989',
-        borderWidth: 2,
-      },
-    ];
-    setData({ labels, datasets });
-    setOption({
-      elements: {},
-      maintainAspectRatio: false,
-      tension: 0.2,
-      // responsive: true,
-      plugins: {
-        tooltip: {
-          interaction: {
-            mode: 'index',
-            axis: 'x',
-          },
-          intersect: false,
-        },
-        legend: {
-          display: false,
-        },
-        title: {
-          display: true,
-          text: stock,
-        },
-      },
-    });
-    setStock('');
-  }
+	async function fetchData() {
+		const apiData = await fetch(
+			`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock}&apikey=${process.env.REACT_APP_API_KEY}`
+		);
+		const convertData = await apiData.json();
+		let labels = [];
+		let data = [];
+		let dataYear = await convertData['Time Series (Daily)'];
+		for (const [key, val] of Object.entries(dataYear)) {
+			labels.unshift(key);
+			data.unshift(
+				(parseFloat(val['1. open']) + parseFloat(val['3. low'])) / 2
+			);
+		}
+		let datasets = [
+			{
+				label: 'Price',
+				data: data,
+				backgroundColor: ['#ef8e19'],
+				borderColor: '#006989',
+				borderWidth: 2,
+			},
+		];
+		setData({ labels, datasets });
+		setOption({
+			elements: {},
+			maintainAspectRatio: false,
+			tension: 0.2,
+			// responsive: true,
+			plugins: {
+				tooltip: {
+					interaction: {
+						mode: 'index',
+						axis: 'x',
+					},
+					intersect: false,
+				},
+				legend: {
+					display: false,
+				},
+				title: {
+					display: true,
+					text: stock,
+				},
+			},
+		});
+		setStock('');
+	}
 
-  function handleChoose() {
-    fetchData();
-  }
+	function handleChoose() {
+		fetchData();
+	}
 
-  return (
-    <BrowserRouter>
-      <MainPageContext.Provider
-        value={{
-          options,
-          data,
-          stock,
-          setStock,
-          dataOption,
-          handleChoose,
-          handleSignOut,
-          fetchData
-        }}
-      >
-        <Routes>
-          <Route path="/" element={<ShareLayout />}>
-            <Route
-              index
-              element={
-                <ProtectedRoute user={user}>
-                  <Home />
-                </ProtectedRoute>
-              }
-            />
-            
-          </Route>
-          <Route
-              path="login"
-              element={
-                <Login
-                  setUser={setUser}
-                />
-              }
-            />
-            <Route
-              path="signup"
-              element={
-                <Signup
-                  setUser={setUser}
-                />
-              }
-            />
-        </Routes>
-      </MainPageContext.Provider>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<MainPageContext.Provider
+				value={{
+					options,
+					data,
+					stock,
+					setStock,
+					dataOption,
+					handleChoose,
+					handleSignOut,
+					fetchData,
+				}}
+			>
+				<Routes>
+					<Route path="/" element={<ShareLayout />}>
+						<Route
+							index
+							element={
+								<ProtectedRoute user={user}>
+									<Home />
+								</ProtectedRoute>
+							}
+						/>
+					</Route>
+					<Route path="login" element={<Login setUser={setUser} />} />
+					<Route path="signup" element={<Signup setUser={setUser} />} />
+				</Routes>
+			</MainPageContext.Provider>
+		</BrowserRouter>
+	);
 }
 
 export default App;
