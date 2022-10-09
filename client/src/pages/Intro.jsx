b8acdd199be15c17460ef103978d57c268a4b922
Minh Vy ha <minhvy.ha@outlook.com>
2022-10-09 21:57:56 +1100

    update

--- client/src/pages/Intro.jsx
+++ client/src/pages/Intro.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../assets/images/textLogo.png';
+import Logo from '../assets/images/Logo.png';
 
 function Intro() {
 	return (
 		<div className="intro-container">
-			<img src={Logo} alt="" />
+			<img className='intro-image' src={Logo} alt="" />
 			<div className="intro-form-container">
 				<h1 className="intro-title">Welcome to StockX</h1>
 				<p className="intro-subtitle">
