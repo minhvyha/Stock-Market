6272919a870b087e533fce2278adeab241a9a303
Minh Vy ha <minhvy.ha@outlook.com>
2022-10-09 21:49:00 +1100

    update

--- client/src/pages/ShareLayout.jsx
+++ client/src/pages/ShareLayout.jsx
@@ -3,12 +3,12 @@ import { Outlet } from 'react-router-dom';
 import Nav from '../components/Nav';
 
 function ShareLayout() {
-    return (
-        <>
-            <Nav />
-            <Outlet />
-        </>
-    );
+	return (
+		<>
+			<Nav />
+			<Outlet />
+		</>
+	);
 }
 
 export default ShareLayout;
