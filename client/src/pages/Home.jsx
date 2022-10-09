81160b51936afedb2042c79128022ce1df7f3574
Minh Vy ha <minhvy.ha@outlook.com>
2022-10-09 20:19:52 +1100

    update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,43 +4,43 @@ import { MainPageContext } from '../App';
 import { Link } from 'react-router-dom';
 
 function Home() {
-  const {
-    options,
-    data,
-    stock,
-    setStock,
-    dataOption,
-    handleChoose,
-    handleSignOut,
-    fetchData,
-  } = useContext(MainPageContext);
-  useEffect(() => {
-    fetchData();
-  }, []);
-  return (
-    <div className="main-container">
-      <div className="chart-container">
-        <LineChart options={options} data={data} fontSize={9} />
-      </div>
-      <label htmlFor="symbolList">Choose a symbol</label>
-      <input
-        type="text"
-        name="symbolList"
-        id="symbolList"
-        list="symbolData"
-        className="symbol-list-box"
-        value={stock}
-        onChange={(e) => setStock(e.target.value)}
-      />
-      <datalist id="symbolData">{dataOption}</datalist>
-      <button onClick={handleChoose}>See Price</button>
-      <Link to="/login">
-        <button className="btn-sign-out" onClick={handleSignOut}>
-          Sign Out
-        </button>
-      </Link>
-    </div>
-  );
+    const {
+        options,
+        data,
+        stock,
+        setStock,
+        dataOption,
+        handleChoose,
+        handleSignOut,
+        fetchData,
+    } = useContext(MainPageContext);
+    useEffect(() => {
+        fetchData();
+    }, []);
+    return (
+        <div className="main-container">
+            <div className="chart-container">
+                <LineChart options={options} data={data} fontSize={9} />
+            </div>
+            <label htmlFor="symbolList">Choose a symbol</label>
+            <input
+                type="text"
+                name="symbolList"
+                id="symbolList"
+                list="symbolData"
+                className="symbol-list-box"
+                value={stock}
+                onChange={(e) => setStock(e.target.value)}
+            />
+            <datalist id="symbolData">{dataOption}</datalist>
+            <button onClick={handleChoose}>See Price</button>
+            <Link to="/login">
+                <button className="btn-sign-out" onClick={handleSignOut}>
+                    Sign Out
+                </button>
+            </Link>
+        </div>
+    );
 }
 
 export default Home;
