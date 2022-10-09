56f41a233f8542578310991af0494b4b59b0739d
Minh Vy ha <minhvy.ha@outlook.com>
2022-10-09 20:31:12 +1100

    udpate

--- client/src/pages/Intro.jsx
+++ client/src/pages/Intro.jsx
@@ -6,6 +6,16 @@ function Intro() {
     return (
         <div className="intro-container">
             <img src={Logo} alt="" />
+            <div className='intro-form-container'>
+                <h1 className='intro-title'>Welcome to StockX</h1>
+                <p className='intro-subtitle'>Exchange, trade, monitor<br></br>stocks and cryptos.</p>
+                <Link to="/login">
+                    <button className='intro-login-btn'>Log In</button>
+                </Link>
+                <Link to="/signup">
+                    <button className='intro-signup-btn'>Sign UP</button>
+                </Link>
+            </div>
         </div>
     );
 }
