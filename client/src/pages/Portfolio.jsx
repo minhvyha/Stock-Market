0d76aebcdaa837b7218bcfdbc6965d93ef9677e1
Minh Vy ha <minhvy.ha@outlook.com>
2022-10-14 19:29:07 +1100

    update

--- client/src/pages/Portfolio.jsx
+++ client/src/pages/Portfolio.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Link} from 'react-router-dom'
- 
+import { Link } from 'react-router-dom';
+
 function Portfolio() {
-	return <>
-   <Link to="/intro">
+	return (
+		<>
+			<Link to="/intro">
 				<button className="btn-sign-out" onClick={handleSignOut}>
 					Sign Out
 				</button>
 			</Link>
-   </>;
+		</>
+	);
 }
 
 export default Portfolio;
