026e2cb3f9d93d692d7d345bc7cd2d94d47a3187
Minh Vy ha <minhvy.ha@outlook.com>
2022-10-14 19:31:06 +1100

    update

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import {MainPageContext} from '../App'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MainPageContext } from '../App';
 
 function Account() {
-   
-  return (
-    <>
-    <Link to="/intro">
+	return (
+		<>
+			<Link to="/intro">
 				<button className="btn-sign-out" onClick={handleSignOut}>
 					Sign Out
 				</button>
 			</Link>
-    </>
-  )
+		</>
+	);
 }
 
-export default Account
+export default Account;
