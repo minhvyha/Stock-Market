1e3fb868105429cd749175566c5fbc6887f98419
Minh Vy ha <minhvy.ha@outlook.com>
2022-10-15 11:56:40 +1100

    update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import './Footer.css';
 
 function Footer() {
-  return (
-    <div>Footer</div>
-  )
+	return (
+		<footer>
+			<div className="footer-item-container">
+				<span class="material-symbols-outlined">home</span>
+			</div>
+			<div className="footer-item-container">
+				<span class="material-symbols-outlined">
+					account_balance_wallet
+				</span>
+			</div>
+			<div className="footer-item-container">
+				<span class="material-symbols-outlined">person</span>
+			</div>
+		</footer>
+	);
 }
 
-export default Footer
+export default Footer;
--- client/src/pages/Layout/ShareLayout.jsx
+++ client/src/pages/Layout/ShareLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Nav from '../../components/Nav';
+import Footer from '../../components/Footer';
 
 function ShareLayout() {
 	return (
 		<>
 			<Nav />
 			<Outlet />
+			<Footer />
 		</>
 	);
 }
