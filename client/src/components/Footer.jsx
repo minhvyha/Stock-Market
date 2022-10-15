34cf947ebe6c1da24aeaa656cb8bb99bbfd127b3
Minh Vy ha <minhvy.ha@outlook.com>
2022-10-15 12:04:48 +1100

    udpate

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -10,22 +10,28 @@ function Footer() {
 				<span class="material-symbols-outlined">home</span>
 			</div>
          </Link>
-         <Link>
+         <Link to='/portfolio'>
 			<div className="footer-item-container">
 				<span class="material-symbols-outlined">
 					account_balance_wallet
 				</span>
 			</div>
          </Link>
+         <Link to='/buy'>
 			<div className="footer-item-container">
 				<span class="material-symbols-outlined">paid</span>
 			</div>
+         </Link>
+         <Link to='/sell'>
 			<div className="footer-item-container">
 				<span class="material-symbols-outlined">trending_down</span>
 			</div>
+         </Link>
+         <Link to='/account'>
 			<div className="footer-item-container">
 				<span class="material-symbols-outlined">person</span>
 			</div>
+         </Link>
 		</footer>
 	);
 }
