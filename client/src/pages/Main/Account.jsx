2151c43e618384c083b9868c38ac6dcf8538d8cf
Minh Vy ha <minhvy.ha@outlook.com>
2022-10-20 22:30:22 +1100

    update

--- client/src/pages/Main/Account.jsx
+++ client/src/pages/Main/Account.jsx
@@ -4,17 +4,22 @@ import { MainPageContext } from '../../App';
 
 function Account() {
 	const { handleSignOut, user } = useContext(MainPageContext);
+	function
 	return (
 		<div className="main-container">
 			<div className="sign-out-container">
 				<h2 className="sign-out-name">{user.name}</h2>
-				<Link to="/intro">
-					<button className="btn-sign-out" onClick={handleSignOut}>
+				<button className="btn-sign-out" onClick={handleSignOut}>
 						Sign Out
-					</button>
-				</Link>
+				</button>
 				<div className='sign-out-form'>
-					
+					<h4>Are you sure you want to sign out?</h4>
+					<button>CANCEL</button>
+					<Link to="/intro">
+						<button onClick={handleSignOut}>
+							SIGN OUT
+						</button>
+					</Link>
 				</div>
 			</div>
 		</div>
