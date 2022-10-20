ac898b635de64d20cf45dde0bf46c93ad1c16e90
Minh Vy ha <minhvy.ha@outlook.com>
2022-10-20 22:18:13 +1100

    update

--- client/src/pages/Main/Buy.jsx
+++ client/src/pages/Main/Buy.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
-
+import React from 'react';
 
 function Buy() {
-  return (
-    <div className='main-container'>Buy</div>
-  )
+	return <div className="main-container">Buy</div>;
 }
 
-export default Buy
+export default Buy;
