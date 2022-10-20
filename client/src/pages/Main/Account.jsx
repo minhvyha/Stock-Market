688a34d3383bf522ecf506a20f8fd2a231b41936
Minh Vy ha <minhvy.ha@outlook.com>
2022-10-20 22:33:46 +1100

    update

--- client/src/pages/Main/Account.jsx
+++ client/src/pages/Main/Account.jsx
@@ -4,26 +4,24 @@ import { MainPageContext } from '../../App';
 
 function Account() {
 	const { handleSignOut, user } = useContext(MainPageContext);
-	function handlePopUp(){
-		document.getElementById('sign-out-form').style.display = 'block'
+	function handlePopUp() {
+		document.getElementById('sign-out-form').style.display = 'block';
 	}
-	function handleCancel(){
-		document.getElementById('sign-out-form').style.display = 'none'
+	function handleCancel() {
+		document.getElementById('sign-out-form').style.display = 'none';
 	}
 	return (
 		<div className="main-container">
 			<div className="sign-out-container">
 				<h2 className="sign-out-name">{user.name}</h2>
 				<button className="btn-sign-out" onClick={handlePopUp}>
-						Sign Out
+					Sign Out
 				</button>
-				<div id='sign-out-form'>
+				<div id="sign-out-form">
 					<h4>Are you sure you want to sign out?</h4>
 					<button onClick={handleCancel}>CANCEL</button>
 					<Link to="/intro">
-						<button onClick={handleSignOut}>
-							SIGN OUT
-						</button>
+						<button onClick={handleSignOut}>SIGN OUT</button>
 					</Link>
 				</div>
 			</div>
