16e52fea513a51904a5eb088f50a9296444d16f0
Minh Vy ha <minhvy.ha@outlook.com>
2022-12-19 09:48:23 +1100

    update

--- client/src/pages/Main/Buy.jsx
+++ client/src/pages/Main/Buy.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './Buy.css'
+import BuyImage from '../../assets/images/StackPaper.png'
 
 function Buy() {
-	return <div className="main-container">Buy</div>;
+	return <div className="main-container">
+		<div>
+			<img className='buy-image' src={BuyImage} alt="buy image" />
+		</div>
+	</div>;
 }
 
 export default Buy;
