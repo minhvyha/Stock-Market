d7bd6539da543a1ddd09767b92037ddfd74cdde5
Minh Vy ha <minhvy.ha@outlook.com>
2022-12-19 21:10:51 +1100

    update

--- client/src/pages/Main/Buy.jsx
+++ client/src/pages/Main/Buy.jsx
@@ -5,18 +5,21 @@ import BuyImage from '../../assets/images/BuyImage.png';
 function Buy() {
 	return (
 		<div className="main-container">
-			<div className='buy-main-container'>
-			<div>
-				<div className="buy-title">Choose a stock symbol to buy.</div>
-				<img className="buy-image" src={BuyImage} alt="buy image" />
-			</div>
-			<div className="buy-action-container">
+			<div className="buy-main-container">
+				<div>
+					<div className="buy-title">Choose a stock symbol to buy.</div>
+					<img className="buy-image" src={BuyImage} alt="buy image" />
+				</div>
 				<div className="buy-search-container">
-					<div className='buy-search-title'>Find the right stock for you</div>
-					<div className='buy-search-subtitle'>Receive accurated live pricing of the stock based on your buy preference.</div>
+					<div className="buy-search-title">
+						Find the right stock for you
+					</div>
+					<div className="buy-search-subtitle">
+						Receive accurated live pricing of the stock based on your buy
+						preference.
+					</div>
 					<span className="material-symbols-outlined">search</span>
 				</div>
-			</div>
 			</div>
 		</div>
 	);
