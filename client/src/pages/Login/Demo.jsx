09bec39556442e8fbcb161197d86c7efb1a25e48
Minh Vy Ha <vyha0512@gmail.com>
2023-03-22 09:46:52 +1100

    update

--- client/src/pages/Login/Demo.jsx
+++ client/src/pages/Login/Demo.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import './Demo.css';
-import NavIntro from '../../components/NavIntro';
+import React from "react";
+import "./Demo.css";
+import NavIntro from "../../components/NavIntro";
 
 function Demo() {
-	return <div>
-		<NavIntro />
-	</div>;
+  return (
+    <div>
+      <NavIntro />
+    </div>
+  );
 }
 
 export default Demo;
