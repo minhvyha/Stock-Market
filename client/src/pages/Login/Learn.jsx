d727f352b3344ee444e30578a0e7aa67a9fb1bf5
Minh Vy Ha <vyha0512@gmail.com>
2023-03-22 09:47:45 +1100

    udpate

--- client/src/pages/Login/Learn.jsx
+++ client/src/pages/Login/Learn.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import './Learn.css';
-import NavIntro from '../../components/NavIntro';
+import React from "react";
+import "./Learn.css";
+import NavIntro from "../../components/NavIntro";
 
 function Learn() {
-	return <div><NavIntro /></div>;
+  return (
+    <div>
+      <NavIntro />
+    </div>
+  );
 }
 
 export default Learn;
