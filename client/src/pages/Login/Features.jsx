ab316ad41b5fd851679dd024d5acb71e1e0fee0b
Minh Vy Ha <vyha0512@gmail.com>
2023-03-30 11:27:41 +1100

    update

--- client/src/pages/Login/Features.jsx
+++ client/src/pages/Login/Features.jsx
@@ -4,8 +4,9 @@ import NavIntro from '../../components/NavIntro';
 
 function Features() {
 	return (
-		<div>
+		<div className='feature-container'>
 			<NavIntro activePage={'features'} />
+			<div className='feature-title'>All the features you need for<br/> succeeding with your investment</div>
 		</div>
 	);
 }
