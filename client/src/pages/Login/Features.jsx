a72438b0ab8cd95977ddf6f4d769962fb37fde84
Minh Vy Ha <vyha0512@gmail.com>
2023-03-30 11:32:02 +1100

    udpate

--- client/src/pages/Login/Features.jsx
+++ client/src/pages/Login/Features.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import './Features.css';
-import NavIntro from '../../components/NavIntro';
+import React from "react";
+import "./Features.css";
+import NavIntro from "../../components/NavIntro";
 
 function Features() {
-	return (
-		<div className='feature-container'>
-			<NavIntro activePage={'features'} />
-			<div className='feature-title'>All the features you need for<br/> succeeding with your investment</div>
-		</div>
-	);
+  return (
+    <div className="feature-container">
+      <NavIntro activePage={"features"} />
+      <div className="feature-title">
+        All the features you need for
+        <br /> succeeding with your investment.
+      </div>
+    </div>
+  );
 }
 
 export default Features;
