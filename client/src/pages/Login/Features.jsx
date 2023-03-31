d623761ed44d458b61b72db5883ca8970028bd51
Minh Vy ha <minhvy.ha@outlook.com>
2023-03-31 19:16:16 +1100

    update features

--- client/src/pages/Login/Features.jsx
+++ client/src/pages/Login/Features.jsx
@@ -10,6 +10,13 @@ function Features() {
         All the features you need for
         <br /> succeeding with your investment.
       </div>
+      <div className="feature-content">
+        <div className="feature">
+        <i class="fa-solid fa-chart-line"></i>
+        </div>
+        <div className="feature"></div>
+        <div className="feature"></div>
+      </div>
     </div>
   );
 }
