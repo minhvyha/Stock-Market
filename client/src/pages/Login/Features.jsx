33f4b87c934952bcd9ba7f91dd9ffe78ffc7bc29
Minh Vy ha <minhvy.ha@outlook.com>
2023-03-31 19:20:05 +1100

    update

--- client/src/pages/Login/Features.jsx
+++ client/src/pages/Login/Features.jsx
@@ -1,21 +1,31 @@
-import React from "react";
-import "./Features.css";
-import NavIntro from "../../components/NavIntro";
+import React from 'react';
+import './Features.css';
+import NavIntro from '../../components/NavIntro';
 
 function Features() {
   return (
     <div className="feature-container">
-      <NavIntro activePage={"features"} />
+      <NavIntro activePage={'features'} />
       <div className="feature-title">
         All the features you need for
         <br /> succeeding with your investment.
       </div>
       <div className="feature-content">
         <div className="feature">
-        <i class="fa-solid fa-chart-line"></i>
+          <i class="fa-solid fa-chart-line"></i>
+          <div className="content-title"></div>
+          <div className="content-description"></div>
         </div>
-        <div className="feature"></div>
-        <div className="feature"></div>
+        <div className="feature">
+          <i class="fa-solid fa-chart-line"></i>
+          <div className="content-title"></div>
+          <div className="content-description"></div>
+        </div>
+        <div className="feature">
+          <i class="fa-solid fa-chart-line"></i>
+          <div className="content-title"></div>
+          <div className="content-description"></div>
+        </div>
       </div>
     </div>
   );
