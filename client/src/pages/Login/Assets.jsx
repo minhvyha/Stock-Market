397e8b5ec7226fc7c9ef641e920581db2736fd6a
Minh Vy ha <minhvy.ha@outlook.com>
2023-04-10 12:32:48 +1000

    update

--- client/src/pages/Login/Assets.jsx
+++ client/src/pages/Login/Assets.jsx
@@ -1,18 +1,26 @@
-import React from "react";
-import "./Assets.css";
-import NavIntro from "../../components/NavIntro";
-import CryptoMarket from "../../components/CryptoMarket";
+import React from 'react';
+import './Assets.css';
+import NavIntro from '../../components/NavIntro';
+import CryptoMarket from '../../components/CryptoMarket';
+import StockMarket from '../../components/StockMarket';
 
 function Assets() {
   return (
     <div>
       <NavIntro activePage={'assets'} />
-      <div className="assets-crypto-container">
-        <div className="crypto-market-container">
-        <CryptoMarket/>
+      <div className="assets-container">
+        <div className="assets-crypto-container">
+          <div className="assets-title">Crypto Market Overview</div>
+          <div className="crypto-container">
+            <CryptoMarket />
+          </div>
         </div>
-      </div>
-      <div className="assets-stock-container">
+        <div className="assets-stock-container">
+          <div className="assets-title">Stock Market Overview</div>
+          <div className="stock-container">
+            <StockMarket />
+          </div>
+        </div>
       </div>
     </div>
   );
