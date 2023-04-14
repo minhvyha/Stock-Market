365a3e6b52c9d2b0f846c6d7617d46fc8f632b08
Minh Vy ha <minhvy.ha@outlook.com>
2023-04-14 13:43:42 +1000

    update

--- client/src/pages/Login/News.jsx
+++ client/src/pages/Login/News.jsx
@@ -13,15 +13,34 @@ function News() {
       const responseStock = await fetch(
         `https://api.marketaux.com/v1/news/all?exchanges=NYSE,NASDAQ&filter_entities=true&limit=3&published_after=2023-04-10T03:15&api_token=${process.env.REACT_APP_NEWS_API}`
       );
-      const responseBitcoin = await fetch(
-        `https://newsapi.org/v2/everything?q=bitcoin&apiKey=${process.env.REACT_APP_CRYPTO_NEWS_API}&language=en`
-      )
+
       const responseEthereum = await fetch(
-        `https://newsapi.org/v2/everything?q=ethereum&apiKey=${process.env.REACT_APP_CRYPTO_NEWS_API}&language=en`
+        `https://newsapi.org/v2/everything?q=crypto&apiKey=${process.env.REACT_APP_CRYPTO_NEWS_API}&language=en`
       )
-      
-      const responseJson = await responseStock.json();
-      let stockNewsContent = responseJson['data'].map((info) => {
+      const ethereumtResponseJson = await responseEthereum.json()
+      let cryptoNewsContent = []
+      let count = 0
+      while (count < 6){
+        console.log(1)
+        let curNews = ethereumtResponseJson['articles'].shift()
+        if ( curNews.urlToImage === undefined || curNews.urlToImage === null){
+          continue
+        }
+        cryptoNewsContent.unshift(
+          <NewsCard key={nanoid()}
+          title={curNews.title}
+          description={curNews.description}
+          imageUrl={curNews.urlToImage}
+          url={curNews.url}
+          alt={curNews.author}
+          time={curNews.publishedAt} />
+        )
+        count++
+      }
+
+
+      const stockResponseJson = await responseStock.json();
+      let stockNewsContent = stockResponseJson['data'].map((info) => {
         return (
           <NewsCard
             key={info.uuid}
@@ -34,6 +53,8 @@ function News() {
           />
         );
       });
+      console.log(cryptoNewsContent)
+      setCryptoNews(cryptoNewsContent)
       setStockNews(stockNewsContent);
     }
     fetchData();
@@ -43,6 +64,7 @@ function News() {
     <div className="news-wrapper">
       <NavIntro activePage={'news'} />
       <div className="news-cards">{stockNews}</div>
+      {cryptoNews}
     </div>
   );
 }
