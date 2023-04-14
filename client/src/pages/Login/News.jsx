6aedf2f225ccc36a1cd52087ce95c668daa88850
Minh Vy ha <minhvy.ha@outlook.com>
2023-04-14 13:20:54 +1000

    udpate

--- client/src/pages/Login/News.jsx
+++ client/src/pages/Login/News.jsx
@@ -5,15 +5,23 @@ import NewsCard from '../../components/NewsCard';
 import NavIntro from '../../components/NavIntro';
 
 function News() {
-  let [news, setNews] = useState();
+  let [stockNews, setStockNews] = useState();
+  let [cryptoNews, setCryptoNews] = useState();
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch(
+      const responseStock = await fetch(
         `https://api.marketaux.com/v1/news/all?exchanges=NYSE,NASDAQ&filter_entities=true&limit=3&published_after=2023-04-10T03:15&api_token=${process.env.REACT_APP_NEWS_API}`
       );
-      const responseJson = await response.json();
-      let newsContent = responseJson['data'].map((info) => {
+      const responseBitcoin = await fetch(
+        `https://newsapi.org/v2/everything?q=bitcoin&apiKey=${process.env.REACT_APP_CRYPTO_NEWS_API}&language=en`
+      )
+      const responseEthereum = await fetch(
+        `https://newsapi.org/v2/everything?q=ethereum&apiKey=${process.env.REACT_APP_CRYPTO_NEWS_API}&language=en`
+      )
+      
+      const responseJson = await responseStock.json();
+      let stockNewsContent = responseJson['data'].map((info) => {
         return (
           <NewsCard
             key={info.uuid}
@@ -26,7 +34,7 @@ function News() {
           />
         );
       });
-      setNews(newsContent);
+      setStockNews(stockNewsContent);
     }
     fetchData();
   }, []);
@@ -34,7 +42,7 @@ function News() {
   return (
     <div className="news-wrapper">
       <NavIntro activePage={'news'} />
-      <div className="news-cards">{news}</div>
+      <div className="news-cards">{stockNews}</div>
     </div>
   );
 }
