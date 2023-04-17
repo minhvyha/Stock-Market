c5903350bd56902a0db2294138c7b2ebbb824716
Minh Vy ha <minhvy.ha@outlook.com>
2023-04-17 20:18:45 +1000

    update

--- client/src/pages/Login/Demo.jsx
+++ client/src/pages/Login/Demo.jsx
@@ -7,10 +7,14 @@ function Demo() {
     <div>
       <NavIntro activePage={"demo"} />
       <div className="demo-signup-container">
-
+      <div className="demo-title">
+      Futuris Demo Trading
+      </div>
       </div>
       <div className="demo-instruction-container">
-
+      <div className="demo-title">
+      How to Use Demo Mode
+      </div>
       </div>
     </div>
   );
