9664a33068f1a313d37d5cda0e8c9bd8f856728e
Minh Vy ha <minhvy.ha@outlook.com>
2023-04-17 20:18:51 +1000

    update

--- client/src/pages/Login/Demo.jsx
+++ client/src/pages/Login/Demo.jsx
@@ -1,20 +1,16 @@
-import React from "react";
-import "./Demo.css";
-import NavIntro from "../../components/NavIntro";
+import React from 'react';
+import './Demo.css';
+import NavIntro from '../../components/NavIntro';
 
 function Demo() {
   return (
     <div>
-      <NavIntro activePage={"demo"} />
+      <NavIntro activePage={'demo'} />
       <div className="demo-signup-container">
-      <div className="demo-title">
-      Futuris Demo Trading
-      </div>
+        <div className="demo-title">Futuris Demo Trading</div>
       </div>
       <div className="demo-instruction-container">
-      <div className="demo-title">
-      How to Use Demo Mode
-      </div>
+        <div className="demo-title">How to Use Demo Mode</div>
       </div>
     </div>
   );
