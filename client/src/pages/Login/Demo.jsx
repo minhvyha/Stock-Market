bf4e09a52103561db229413235d03260e5bd8d6a
Minh Vy ha <minhvy.ha@outlook.com>
2023-04-19 14:38:19 +1000

    update

--- client/src/pages/Login/Demo.jsx
+++ client/src/pages/Login/Demo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Demo.css';
 import NavIntro from '../../components/NavIntro';
+import { Link } from 'react-router-dom';
 
 function Demo() {
   return (
@@ -8,6 +9,12 @@ function Demo() {
       <NavIntro activePage={'demo'} />
       <div className="demo-signup-container">
         <div className="demo-title">Futuris Demo Trading</div>
+        <div className='demo-subtitle'>Demo feature allows you to trade stock and crypto with <br />real-time market data and risk free.</div>
+        <Link to="/signup">
+						<button className="intro-signup-btn intro-main-btn">
+							Get started
+						</button>
+					</Link>
       </div>
       <div className="demo-instruction-container">
         <div className="demo-title">How to Use Demo Mode</div>
