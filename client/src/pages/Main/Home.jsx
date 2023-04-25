b7bc6762a12f7a6ff367753f9d3e1b3537cbc5e0
Minh Vy ha <minhvy.ha@outlook.com>
2023-04-25 19:08:55 +1000

    update

--- client/src/pages/Main/Home.jsx
+++ client/src/pages/Main/Home.jsx
@@ -1,25 +1,34 @@
 import React, { useContext } from 'react';
 import { MainPageContext } from '../../App';
 import './Home.css';
-import TradingViewWidget from '../../components/TradingView';
+import { AdvancedRealTimeChart } from 'react-ts-tradingview-widgets';
 import Selection from '../../components/Selection';
 
 function Home() {
-	const { stock } = useContext(MainPageContext);
-	return (
-		<div className="main-container">
-			<div id="home-chart-container">
-				<TradingViewWidget symbol={stock} />
-			</div>
-			<div className="side-container">
-				<Selection />
-				<div className="action-container">
-					<div className="action-buy">Buy</div>
-					<div className="action-sell">Sell</div>
-				</div>
-			</div>
-		</div>
-	);
+  const { stock } = useContext(MainPageContext);
+  return (
+    <div className="main-container">
+      <div id="home-chart-container">
+        <AdvancedRealTimeChart
+          symbol={stock}
+          copyrightStyles={{
+            parent: {
+              fontSize: '0px',
+              display: 'none',
+            },
+          }}
+          theme="dark"
+        />
+      </div>
+      <div className="side-container">
+        <Selection />
+        <div className="action-container">
+          <div className="action-buy">Buy</div>
+          <div className="action-sell">Sell</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
