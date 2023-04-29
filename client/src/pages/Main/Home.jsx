c85197580b867256b8ca2802995d441cfc562985
Minh Vy ha <minhvy.ha@outlook.com>
2023-04-29 16:10:38 +1000

    update

--- client/src/pages/Main/Home.jsx
+++ client/src/pages/Main/Home.jsx
@@ -1,6 +1,9 @@
 import React, { useContext, useState } from 'react';
 import { MainPageContext } from '../../App';
 import './Home.css';
+import { TechnicalAnalysis } from "react-ts-tradingview-widgets";
+import { FundamentalData } from "react-ts-tradingview-widgets";
+import { CompanyProfile } from "react-ts-tradingview-widgets";
 import { AdvancedRealTimeChart } from 'react-ts-tradingview-widgets';
 import Selection from '../../components/Selection';
 
@@ -9,34 +12,46 @@ function Home() {
 
   return (
     <div className="main-container">
+      <div className="main-wrapper">
+        <div className="home-chart-wrapper">
+          <div className="home-chart-description">
+            {title} ({stock}) - {sector}
+          </div>
+          <div id="home-chart-container">
+            <AdvancedRealTimeChart
+              hide_side_toolbar={true}
+              autosize={true}
+              allow_symbol_change={false}
+              symbol={stock}
+              copyrightStyles={{
+                parent: {
+                  fontSize: '0px',
+                  display: 'none',
+                },
+              }}
+              theme="dark"
+            />
+          </div>
+        </div>
+        <div className="side-container">
+          <Selection />
+          <div className="action-container">
+            <div className="action-buy">Buy</div>
+            <div className="action-sell">Sell</div>
+          </div>
+        </div>
+      </div>
       <div className='main-wrapper'>
-      <div className="home-chart-wrapper">
-        <div className="home-chart-description">
-          {title} ({stock}) - {sector}
-        </div>
-        <div id="home-chart-container">
-          <AdvancedRealTimeChart
-            hide_side_toolbar={true}
-            autosize={true}
-            allow_symbol_change={false}
-            symbol={stock}
-            copyrightStyles={{
-              parent: {
-                fontSize: '0px',
-                display: 'none',
-              },
-            }}
-            theme="dark"
-          />
-        </div>
-      </div>
-      <div className="side-container">
-        <Selection />
-        <div className="action-container">
-          <div className="action-buy">Buy</div>
-          <div className="action-sell">Sell</div>
-        </div>
-      </div>
+              <div className='symbol-info-title'>
+                Symbol Profile
+              </div>
+        <CompanyProfile colorTheme="dark" copyrightStyles={{
+                parent: {
+                  fontSize: '0px',
+                  display: 'none',
+                },
+              }} height={400} width="100%"></CompanyProfile>
+
       </div>
     </div>
   );
