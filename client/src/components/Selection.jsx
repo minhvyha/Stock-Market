83827a23edb7af6f5ace36635bc9aa866ac7d2bf
Minh Vy ha <minhvy.ha@outlook.com>
2023-04-30 09:25:23 +1000

    updsate

--- client/src/components/Selection.jsx
+++ client/src/components/Selection.jsx
@@ -5,92 +5,90 @@ import { nanoid } from 'nanoid';
 import './Selection.css';
 
 function Selection({ handleChoose }) {
-	const [isDropDown, setIsDropDown] = useState(false);
-	const { setStock, setTitle, setSector } = useContext(MainPageContext);
+  const [isDropDown, setIsDropDown] = useState(false);
+  const { setStock, setTitle, setSector } = useContext(MainPageContext);
 
-	function selectBoxClick() {
-		const optionsContainer = document.querySelector('.options-container');
-		const searchBox = document.querySelector('.search-box input');
-		setIsDropDown((value) => !value);
-		searchBox.value = '';
-		filterList('');
+  function selectBoxClick() {
+    const optionsContainer = document.querySelector('.options-container');
+    const searchBox = document.querySelector('.search-box input');
+    setIsDropDown((value) => !value);
+    searchBox.value = '';
+    filterList('');
 
-		if (!optionsContainer.classList.contains('active')) {
-			searchBox.focus();
-		}
-	}
+    if (!optionsContainer.classList.contains('active')) {
+      searchBox.focus();
+    }
+  }
 
-	let optionList = Symbol.map((company) => {
-		return (
-			<div
-				key={nanoid()}
-				className="option"
-				onClick={() => {
-					setStock(company.Symbol);
-					setTitle(company.Name)
-					setSector(company.Sector)
+  let optionList = Symbol.map((company) => {
+    return (
+      <div
+        key={nanoid()}
+        className="option"
+        onClick={() => {
+          setStock(company.Symbol);
+          setTitle(company.Name);
+          setSector(company.Sector);
 
-					document.querySelector(
-						'.selected'
-					).innerHTML = `${company.Symbol}`;
-					setIsDropDown((value) => !value);
-					handleChoose();
-				}}
-			>
-				<input
-					type="radio"
-					className="radio"
-					id={`${company.Symbol}`}
-					name="category"
-				/>
-				<label
-					htmlFor={`${company.Symbol}`}
-				>{`${company.Name} - ${company.Symbol}`}</label>
-			</div>
-		);
-	});
+          document.querySelector('.selected').innerHTML = `${company.Symbol}`;
+          setIsDropDown((value) => !value);
+          handleChoose();
+        }}
+      >
+        <input
+          type="radio"
+          className="radio"
+          id={`${company.Symbol}`}
+          name="category"
+        />
+        <label
+          htmlFor={`${company.Symbol}`}
+        >{`${company.Name} - ${company.Symbol}`}</label>
+      </div>
+    );
+  });
 
-	const filterList = (searchTerm) => {
-		const optionsList = document.querySelectorAll('.option');
-		searchTerm = searchTerm.toLowerCase();
-		optionsList.forEach((option) => {
-			let label =
-				option.firstElementChild.nextElementSibling.innerText.toLowerCase();
-			if (label.indexOf(searchTerm) !== -1) {
-				option.style.display = 'block';
-			} else {
-				option.style.display = 'none';
-			}
-		});
-	};
+  const filterList = (searchTerm) => {
+    const optionsList = document.querySelectorAll('.option');
+    searchTerm = searchTerm.toLowerCase();
+    optionsList.forEach((option) => {
+      let label =
+        option.firstElementChild.nextElementSibling.innerText.toLowerCase();
+      if (label.indexOf(searchTerm) !== -1) {
+        option.style.display = 'block';
+      } else {
+        option.style.display = 'none';
+      }
+    });
+  };
 
-	return (
-		<div className="choose-input-container">
-			<div className="select-box">
-				<div
-					className={
-						isDropDown ? 'options-container active' : 'options-container'
-					}
-				>
-					{optionList}
-				</div>
+  return (
+    <div className="choose-input-container">
+      <div className="select-box">
+        <div
+          className={
+            isDropDown ? 'options-container active' : 'options-container'
+          }
+        >
+          {optionList}
+        </div>
 
-				<div className="selected" onClick={selectBoxClick}>
-					Select Symbol
-				</div>
+        <div className="selected" onClick={selectBoxClick}>
+          Select Symbol
+        </div>
 
-				<div className="search-box">
-					<input
-						type="text"
-						placeholder="Start Typing..."
-						onKeyUp={function (e) {
-							filterList(e.target.value);
-						}}
-					/>
-				</div>
-			</div>
-		</div>
-	);
+        <div className="search-box">
+          <input
+            type="text"
+            placeholder="Start Typing..."
+            onKeyUp={function (e) {
+              filterList(e.target.value);
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Selection;
