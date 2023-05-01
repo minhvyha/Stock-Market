adb9e9089e47107fbcf728139600b10b2a81b997
Minh Vy ha <minhvy.ha@outlook.com>
2023-05-01 22:20:33 +1000

    update pie chart

--- client/src/pages/Main/Portfolio.jsx
+++ client/src/pages/Main/Portfolio.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import './Portfolio'
-
+import './Portfolio';
+import PieChart from '../../components/PieChart';
+import { ResponsiveLine } from '@nivo/line'
+import data from '../../MockData';
 
 function Portfolio() {
-	return (
-		<div className="main-container">
-			<h1>Portfolio</h1>
-		</div>
-	);
+  return (
+    <div className="main-container">
+      <h1>Portfolio</h1>
+			<div style={{height: 500}}>
+      <PieChart
+        data={data}
+      />
+			</div>
+    </div>
+  );
 }
 
 export default Portfolio;
