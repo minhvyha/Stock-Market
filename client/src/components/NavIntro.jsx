a47ab28a7e9bc0bca0b9986b889724b54a5ee1db
Minh Vy ha <minhvy.ha@outlook.com>
2023-05-01 22:21:26 +1000

    update

--- client/src/components/NavIntro.jsx
+++ client/src/components/NavIntro.jsx
@@ -6,55 +6,55 @@ import LogoTextDark from '../assets/images/LogoTextDark.png';
 import { Link } from 'react-router-dom';
 
 function NavIntro({ activePage }) {
-	return (
-		<nav className="navBar intro-nav">
-			<div className="nav-container">
-				<input type="checkbox" id="intro-check" />
-				<div className="nav-subcontainer">
-					<Link to="/">
-						<img id="intro-image" className="logo" src={LogoTextDark} alt="" />
-						<img
-							id="intro-image-light"
-							className="logo"
-							src={LogoTextLight}
-							alt=""
-						/>
-					</Link>
-					<label htmlFor="intro-check" className="checkbtn">
-						<i className="fas fa-bars menu-symbol"></i>
-					</label>
-				</div>
+  return (
+    <nav className="navBar intro-nav">
+      <div className="nav-container">
+        <input type="checkbox" id="intro-check" />
+        <div className="nav-subcontainer">
+          <Link to="/">
+            <img id="intro-image" className="logo" src={LogoTextDark} alt="" />
+            <img
+              id="intro-image-light"
+              className="logo"
+              src={LogoTextLight}
+              alt=""
+            />
+          </Link>
+          <label htmlFor="intro-check" className="checkbtn">
+            <i className="fas fa-bars menu-symbol"></i>
+          </label>
+        </div>
 
-				<ul>
-					<li className={activePage === 'home' ? 'active' : ''}>
-						<Link to="/">Home</Link>
-					</li>
-					<li className={activePage === 'assets' ? 'active' : ''}>
-						<Link to="/assets">Assets</Link>
-					</li>
-					<li className={activePage === 'news' ? 'active' : ''}>
-						<Link to="/news">News</Link>
-					</li>
-					<li className={activePage === 'features' ? 'active' : ''}>
-						<Link to="/features">Features</Link>
-					</li>
-					<li className={activePage === 'demo' ? 'active' : ''}>
-						<Link to="/demo">Demo</Link>
-					</li>
-					<li className={activePage === 'login' ? 'active' : ''}>
-						<Link className="login-button" to="/login">
-							Login
-						</Link>
-					</li>
-					<li className={activePage === 'signup' ? 'active' : ''}>
-						<Link className="signup-button" to="/signup">
-							Signup
-						</Link>
-					</li>
-				</ul>
-			</div>
-		</nav>
-	);
+        <ul>
+          <li className={activePage === 'home' ? 'active' : ''}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={activePage === 'assets' ? 'active' : ''}>
+            <Link to="/assets">Assets</Link>
+          </li>
+          <li className={activePage === 'news' ? 'active' : ''}>
+            <Link to="/news">News</Link>
+          </li>
+          <li className={activePage === 'features' ? 'active' : ''}>
+            <Link to="/features">Features</Link>
+          </li>
+          <li className={activePage === 'demo' ? 'active' : ''}>
+            <Link to="/demo">Demo</Link>
+          </li>
+          <li className={activePage === 'login' ? 'active' : ''}>
+            <Link className="login-button" to="/login">
+              Login
+            </Link>
+          </li>
+          <li className={activePage === 'signup' ? 'active' : ''}>
+            <Link className="signup-button" to="/signup">
+              Signup
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default NavIntro;
--- client/src/components/PieChart.jsx
+++ client/src/components/PieChart.jsx
@@ -1,111 +1,99 @@
-import { ResponsivePie } from "@nivo/pie";
+import { ResponsivePie } from '@nivo/pie';
 
-
-const PieChart = ({data}) => {
+const PieChart = ({ data }) => {
   return (
     <ResponsivePie
-        data={data}
-        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-        innerRadius={0.5}
-        padAngle={2}
-        cornerRadius={3}
-        motionConfig={'stiff'}
-        activeOuterRadiusOffset={12}
-        colors={{ scheme: 'oranges' }}
-        borderWidth={2}
-        borderColor={{
-            from: 'color',
-            modifiers: [
-                [
-                    'darker',
-                    0.5
-                ]
-            ]
-        }}
-        enableArcLinkLabels={false}
-        arcLinkLabelsColor={{ from: 'color' }}
-        arcLabel="id"
-        arcLabelsSkipAngle={10}
-        arcLabelsTextColor={{
-            from: 'color',
-            modifiers: [
-                [
-                    'darker',
-                    3
-                ]
-            ]
-        }}
-        defs={[
-            {
-                id: 'dots',
-                type: 'patternDots',
-                background: 'inherit',
-                color: 'rgba(255, 255, 255, 0.3)',
-                size: 4,
-                padding: 1,
-                stagger: true
-            },
-            {
-                id: 'lines',
-                type: 'patternLines',
-                background: 'inherit',
-                color: 'rgba(255, 255, 255, 0.3)',
-                rotation: -45,
-                lineWidth: 6,
-                spacing: 10
-            }
-        ]}
-        fill={[
-            {
-                match: {
-                    id: 'ruby'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'c'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'go'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'python'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'scala'
-                },
-                id: 'lines'
-            },
-            {
-                match: {
-                    id: 'lisp'
-                },
-                id: 'lines'
-            },
-            {
-                match: {
-                    id: 'elixir'
-                },
-                id: 'lines'
-            },
-            {
-                match: {
-                    id: 'javascript'
-                },
-                id: 'lines'
-            }
-        ]}
-      
+      data={data}
+      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+      innerRadius={0.5}
+      padAngle={2}
+      cornerRadius={3}
+      motionConfig={'stiff'}
+      activeOuterRadiusOffset={12}
+      colors={{ scheme: 'oranges' }}
+      borderWidth={2}
+      borderColor={{
+        from: 'color',
+        modifiers: [['darker', 0.5]],
+      }}
+      enableArcLinkLabels={false}
+      arcLinkLabelsColor={{ from: 'color' }}
+      arcLabel="id"
+      arcLabelsSkipAngle={10}
+      arcLabelsTextColor={{
+        from: 'color',
+        modifiers: [['darker', 3]],
+      }}
+      defs={[
+        {
+          id: 'dots',
+          type: 'patternDots',
+          background: 'inherit',
+          color: 'rgba(255, 255, 255, 0.3)',
+          size: 4,
+          padding: 1,
+          stagger: true,
+        },
+        {
+          id: 'lines',
+          type: 'patternLines',
+          background: 'inherit',
+          color: 'rgba(255, 255, 255, 0.3)',
+          rotation: -45,
+          lineWidth: 6,
+          spacing: 10,
+        },
+      ]}
+      fill={[
+        {
+          match: {
+            id: 'ruby',
+          },
+          id: 'dots',
+        },
+        {
+          match: {
+            id: 'c',
+          },
+          id: 'dots',
+        },
+        {
+          match: {
+            id: 'go',
+          },
+          id: 'dots',
+        },
+        {
+          match: {
+            id: 'python',
+          },
+          id: 'dots',
+        },
+        {
+          match: {
+            id: 'scala',
+          },
+          id: 'lines',
+        },
+        {
+          match: {
+            id: 'lisp',
+          },
+          id: 'lines',
+        },
+        {
+          match: {
+            id: 'elixir',
+          },
+          id: 'lines',
+        },
+        {
+          match: {
+            id: 'javascript',
+          },
+          id: 'lines',
+        },
+      ]}
     />
   );
 };
