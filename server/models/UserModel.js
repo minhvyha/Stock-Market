dbe9c316c7f71de11a9729480ede383e474a6780
Minh Vy ha <minhvy.ha@outlook.com>
2023-05-04 19:00:00 +1000

    update

---
+++ server/models/UserModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  assets: {
+    type: mongoose.Schema.Types.Mixed,
+    require: true,
+  },
+  cash: {
+    type: Number,
+    default: 0,
+  },
+  totalAssets: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const UserModel = mongoose.model('note', UserSchema);
+
+module.exports = UserModel;
