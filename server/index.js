7a5919ca810ee6816ada1c676a6bddc91337eef2
Minh Vy ha <minhvy.ha@outlook.com>
2023-05-04 19:00:44 +1000

    update

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
-const {authKey} = require('./middleware/middleware')
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const PORT = process.env.PORT || 3010
+const { authKey } = require('./middleware/middleware');
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 3010;
 
 mongoose.set('strictQuery', false);
 const connectDB = async () => {
@@ -14,47 +14,45 @@ const connectDB = async () => {
     console.log(error);
     process.exit(1);
   }
-}
+};
 
 //Routes go here
-app.get('/:key',[  authKey(process.env.PASSWORD), ], (req,res) => {
-  console.log(req.body)
-    res.send({ title: 'Books', body: 'asdf'});
-})
+app.get('/:key', [authKey(process.env.PASSWORD)], (req, res) => {
+  console.log(req.body);
+  res.send({ title: 'Books', body: 'asdf' });
+});
 
-app.get('/books', async (req,res)=> {
-
+app.get('/books', async (req, res) => {
   const book = await Book.find();
 
   if (book) {
-    res.json(book)
+    res.json(book);
   } else {
-    res.send("Something went wrong.");
+    res.send('Something went wrong.');
   }
-  
 });
 
-app.get('/add-note', async (req,res) => {
+app.get('/add-note', async (req, res) => {
   try {
     await Book.insertMany([
       {
-        title: "Sons Of Anarchy",
-        body: "Body text goes here...",
+        title: 'Sons Of Anarchy',
+        body: 'Body text goes here...',
       },
       {
-        title: "Games of Thrones",
-        body: "Body text goes here...",
-      }
+        title: 'Games of Thrones',
+        body: 'Body text goes here...',
+      },
     ]);
-    res.json({"Data":"Added"})
+    res.json({ Data: 'Added' });
   } catch (error) {
-    console.log("err", + error);
+    console.log('err', +error);
   }
-})
+});
 
 //Connect to the database before listening
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log("listening for requests on Port: "+PORT);
-    })
-})
+  app.listen(PORT, () => {
+    console.log('listening for requests on Port: ' + PORT);
+  });
+});
