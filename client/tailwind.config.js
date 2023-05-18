ac0eb6b06b69bc5df0ea13c47f1ddbe00e0e05ec
Minh Vy ha <minhvy.ha@outlook.com>
2023-05-19 00:25:31 +1000

    update

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        primary: "#050816",
+        secondary: "#aaa6c3",
+        tertiary: "#151030",
+        "black-100": "#100d25",
+        "black-200": "#090325",
+        "white-100": "#f3f3f3",
         'main-color': '#fa8e30',
+
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/herobg.png')",
       },
     },
   },
